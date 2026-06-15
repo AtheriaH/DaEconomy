2f8a15876bb67f8ad5db7ba9d1e3d79dfe4b6eac
Atheria <ayushsukhdeve9@gmail.com>
2026-06-15 23:23:53 +0530

    Update MoneyCommand.php

--- src/DaEconomy/command/MoneyCommand.php
+++ src/DaEconomy/command/MoneyCommand.php
@@ -8,7 +8,7 @@ use DaEconomy\DaEconomy;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\player\Player;
-use pocketmine\utils\TextFormat as TF;
+use pocketmine\utils\TextFormat;
 
 class MoneyCommand extends Command {
 
@@ -17,25 +17,23 @@ class MoneyCommand extends Command {
         $this->setPermission("daeconomy.command.money");
     }
 
-    public function execute(CommandSender $sender, string $commandLabel, array $args): bool {
+    public function execute(CommandSender $sender, string $commandLabel, array $args): void {
         $targetName = $args[0] ?? null;
 
         if ($targetName === null) {
             if (!$sender instanceof Player) {
-                $sender->sendMessage(TF::RED . "Please specify a player name from the console.");
-                return false;
+                $sender->sendMessage(TextFormat::RED . "Please specify a player name from the console.");
+                return;
             }
             $balance = $this->plugin->getBalance($sender->getName());
-            $sender->sendMessage(TF::GREEN . "Your Balance: " . TF::YELLOW . $this->plugin->formatMoney($balance));
-            return true;
+            $sender->sendMessage(TextFormat::GREEN . "Your Balance: " . TextFormat::YELLOW . $this->plugin->formatMoney($balance));
+            return;
         }
 
         $target = $this->plugin->getServer()->getPlayerByPrefix($targetName);
-        $realName = $target !== null ? $target->getName() : $targetName;
+        $realName = $target?->getName() ?? $targetName;
         
         $balance = $this->plugin->getBalance($realName);
-        $sender->sendMessage(TF::GREEN . $realName . "'s Balance: " . TF::YELLOW . $this->plugin->formatMoney($balance));
-        
-        return true;
+        $sender->sendMessage(TextFormat::GREEN . $realName . "'s Balance: " . TextFormat::YELLOW . $this->plugin->formatMoney($balance));
     }
 }
